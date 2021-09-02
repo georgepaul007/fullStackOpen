c234268d3a433b82d0a3ae1bd6adb55cccbec6d7
George Paul <georgepaul07@yahoo.com>
2021-09-02 13:53:10 +0530

    CHanges made

--- fullStackOpen/Part1/1.6/unicafe/src/App.js
+++ fullStackOpen/Part1/1.6/unicafe/src/App.js
@@ -18,18 +18,7 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  // const incGood = () =>{
-  //   console.log(good)
-  //   setGood(good + 1)
-  // }
-  // const incNeutral = () => {
-  //   console.log(neutral)
-  //   setNeutral(neutral + 1)
-  // }
-  // const incBad = () => {
-  //   console.log(bad)
-  //   setBad(bad + 1)
-  // }
+  
   return (
     
     <div>
