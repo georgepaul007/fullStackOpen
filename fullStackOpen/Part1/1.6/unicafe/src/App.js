b30ddd2007e4f04b297652886ad084f6e361b01a
George Paul <georgepaul07@yahoo.com>
2021-09-02 17:04:56 +0530

    Finished unicafe

--- fullStackOpen/Part1/1.6/unicafe/src/App.js
+++ fullStackOpen/Part1/1.6/unicafe/src/App.js
@@ -1,24 +1,86 @@
 import React, { useState } from 'react'
 const Button = ({handle, text}) => {
   return(
+  <>
   <button onClick = {handle}>
     {text}
   </button>
+  </>
   )
 }
 const Display = (props) => {
   return(
-  <p>
+  <>
     {props.text} {props.value}
-  </p>
+  </>
   )
+}
+const StatisticLine = ({text, value}) => {
+  return(
+    <>
+      <td>
+        {text}
+      </td>
+      <td>
+        {value}
+      </td>
+      <br />
+    </>
+  )
+}
+const Statistics = ({good, neutral, bad}) => {
+  if(good || neutral || bad) {
+    return(
+      <>
+        {/* {console.log(good,bad,neutral)}
+        good {good} <br />
+        neutral {neutral} <br/>
+        bad {bad} <br/>
+        all {good} <br />
+        average {(good - bad)/(good+bad+neutral)} <br />
+        positive {(good/(good+bad+neutral)*100)} % <br /> */}
+        <br />
+        <table>
+         <tr>
+           <StatisticLine text="good" value ={good} />
+          </tr>
+          <tr>
+            <StatisticLine text="neutral" value ={neutral} />
+          </tr>
+          <tr>
+            <StatisticLine text="bad" value ={bad} />
+          </tr>
+          <tr>
+            <StatisticLine text = "average" value = {(good - bad)/(good+bad+neutral)} />
+          </tr>
+          <tr>
+            <StatisticLine text = "positive" value = {(good/(good+bad+neutral)*100)} />
+          </tr>
+        </table>
+      </>
+    )
+  }
+  else {
+    return (
+      <>
+        No feedback given
+      </>
+    )
+  }
 }
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  
+  const findAverage = () => {
+    let total = good + bad + neutral;
+    let average = (good - (bad))/total
+    return average
+  }
+  const positivePercentage = () => {
+    return (good/(good+bad+neutral)*100)
+  }
   return (
     
     <div>
@@ -28,17 +90,10 @@ const App = () => {
         <Button  handle = {() => setGood(good + 1)} text = "good" />
         <Button  handle = {() => setNeutral(neutral + 1)} text = "neutral" />
         <Button  handle = {() => setBad(bad + 1)} text = "bad" />
-        {/* <button {incGood}>good</button>
-        <button {incNeutral}>neutral</button>
-        <button {incBad}>bad</button> */}
       </div>
       <div>
         <h1>statistics</h1>
-        <br />
-        <Display value = {good} text = "good" />
-        <Display value = {neutral} text = "neutral" />
-        <Display value = {bad} text = "bad" />
-
+        <Statistics good = {good} neutral = {neutral} bad = {bad}/>
       </div>
     </div>
   )
