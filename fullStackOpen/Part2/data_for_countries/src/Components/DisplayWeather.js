599cd83ef0182912ef1c4874923f0dc2086119e1
George Paul <georgepaul07@yahoo.com>
2021-09-29 11:51:39 +0530

    Fixed Weather

--- fullStackOpen/Part2/data_for_countries/src/Components/DisplayWeather.js
+++ fullStackOpen/Part2/data_for_countries/src/Components/DisplayWeather.js
@@ -3,21 +3,43 @@ import {useEffect, useState} from 'react'
 
 const DisplayWeather = ({country}) => {
     const api_key = process.env.REACT_APP_API_KEY
+    const languages = Object.values(country.languages)
+    const flag = country.flags.svg
     console.log(api_key)
     const [weatherObject, setWeatherObject] = useState([])
     const hook = () => {
         axios
-          .get("http://api.weatherstack.com/current?access_key={0}&query={1}",api_key,country.capital)
+          .get(`http://api.weatherstack.com/current?access_key=${api_key}&query=${country.capital}`)
           .then(response => {
             console.log("promise")
             setWeatherObject(response.data)
           })
+        //   console.log(weatherAPI)
+        //   setWeatherObject(weatherAPI.data)
       }
     useEffect(hook, [])
     console.log(weatherObject)
+    const weatherIcon = weatherObject.current.weather_icons[0]
     return (
         <>
-            <p>HI</p>
+            <p>
+                Capital {country.capital}
+            </p>
+            <h2>languages</h2>
+            <p>
+                
+                {languages.map((language, i ) => <li key = {i}>{language}</li>)}
+            </p>
+            <p>
+                <img src = {flag} />
+            </p>
+            <h1>Weather in {country.capital}</h1>
+            <p>
+                <b>Temperature: </b> {weatherObject.current.temperature} celsius
+                <br />
+                <img src = {weatherIcon} />
+                <b>Wind: </b> {weatherObject.current.wind_speed} mph direction {weatherObject.current.wind_dir}
+            </p>
         </>
     )
 }
